99f17c2c2436b45d8a730c662530f4428f9632ca
IdiosyncraticDragon <lgy807720302@gmail.com>
2018-06-06 20:10:15 +0800

    settings for a successful training of Transformer

--- scripts/wmt/eval_wmt_ende.sh
+++ scripts/wmt/eval_wmt_ende.sh
@@ -9,20 +9,21 @@ fi
 DATA_PATH=$1
 SP_PATH=/usr/local/bin
 
-runconfig=wmt_ende_transformer_4gpu_lr2_ws8000_dur2_0.998
-testset=newstest2017-ende
+runconfig=/home/lgy/deepModels/tf_models/wmt_ende_transformer_4gpu_lr2_ws8000_dur2_0.998
+#testset=newstest2017-ende
+testset=newstest2014-deen
 sl=en
 tl=de
 
 export PATH=$SP_PATH:$PATH
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=7
 
 wget -nc https://raw.githubusercontent.com/OpenNMT/OpenNMT-tf/master/third_party/input-from-sgm.perl
 wget -nc https://raw.githubusercontent.com/OpenNMT/OpenNMT-tf/master/third_party/multi-bleu-detok.perl
 
-perl input-from-sgm.perl < $DATA_PATH/test/$testset-src.$sl.sgm \
-   | spm_encode --model=data/wmt$sl$tl.model > data/$testset-src.$sl
-perl input-from-sgm.perl < $DATA_PATH/test/$testset-ref.$tl.sgm > data/$testset-ref.$tl
+#perl input-from-sgm.perl < $DATA_PATH/test/$testset-src.$sl.sgm \
+#   | spm_encode --model=data/wmt$sl$tl.model > data/$testset-src.$sl
+#perl input-from-sgm.perl < $DATA_PATH/test/$testset-ref.$tl.sgm > data/$testset-ref.$tl
 
 
 if false; then
@@ -32,19 +33,27 @@ if false; then
                            --output_dir=$runconfig/averaged/
 fi
 
-if true; then
+if false; then
   onmt-main infer \
             --model_type Transformer \
             --config config/wmt_ende.yml \
             --checkpoint_path=$runconfig/averaged \
-            --features_file data/$testset-src.$sl \
-            > data/$testset-src.hyp.$tl
+            --features_file $DATA_PATH/$testset-src.$sl \
+            > $DATA_PATH/$testset-src.hyp.$tl
+fi
+if true; then
+  onmt-main infer \
+            --model_type Transformer \
+            --config config/wmt_ende.yml \
+            --checkpoint_path=$runconfig/\
+            --features_file $DATA_PATH/$testset-src.$sl \
+            > $DATA_PATH/$testset-src.hyp.$tl
 fi
 
 if true; then
-  spm_decode --model=data/wmt$sl$tl.model --input_format=piece \
-             < data/$testset-src.hyp.$tl \
-             > data/$testset-src.hyp.detok.$tl
+  ./spm_decode --model=$DATA_PATH/wmt$sl$tl.model --input_format=piece \
+             < $DATA_PATH/$testset-src.hyp.$tl \
+             > $DATA_PATH/$testset-src.hyp.detok.$tl
 
-  perl multi-bleu-detok.perl data/$testset-ref.$tl < data/$testset-src.hyp.detok.$tl
+  perl multi-bleu-detok.perl $DATA_PATH/$testset-ref.$tl < $DATA_PATH/$testset-src.hyp.detok.$tl
 fi
